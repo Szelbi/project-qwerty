076af4ea00d159d6b2547623402dfdd8209d5a66
dgos <dawidgos25@gmail.com>
2023-04-04 18:23:23 +0200

    Tests UuidFactory

--- app/public/index.php
+++ app/public/index.php
@@ -1,8 +1,14 @@
 <?php
 
 use App\Kernel;
+use Ramsey\Uuid\UuidFactory;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+$id = new UuidFactory();
+echo $id->uuid4();
+
+echo "\nHello world!";
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
